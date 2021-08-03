e1503108b05abdbdb5c4e89151e2f41064595fdb
v-sujoshi <v-sujoshi@microsoft.com>
2021-08-03 10:46:36 +0530

    staged changes

--- main/demo.py
+++ main/demo.py
@@ -1,3 +1,4 @@
 def foo():
-    print("This is the change coming from main")
+    print("This is the change coming from main which is overridden")
     print("This is the new change coming from main from other developers")
+    print("This is the change from git_demo")
