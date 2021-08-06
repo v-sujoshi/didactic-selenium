1c6f79977f55891268117fc6ac35f4befde45720
v-sujoshi <v-sujoshi@microsoft.com>
2021-08-06 11:08:37 +0530

    Revert "staged changes"
    This reverts commit e1503108b05abdbdb5c4e89151e2f41064595fdb.

--- main/demo.py
+++ main/demo.py
@@ -1,4 +1,3 @@
 def foo():
-    print("This is the change coming from main which is overridden")
+    print("This is the change coming from main")
     print("This is the new change coming from main from other developers")
-    print("This is the change from git_demo")
